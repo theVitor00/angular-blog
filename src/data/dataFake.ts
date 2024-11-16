fa6eb3eb14efbe834bd5b3e1d92aee15c53b39f0
Pablo Vitor <thevitor00@gmail.com>
2024-11-15 22:03:07 -0300

    Update: novos componentes e funcionalidades adicionados ao blog

---
+++ src/data/dataFake.ts
@@ -0,0 +1,32 @@
+export const dataFake = [
+  {
+    "id": "1",
+    "photo": "https://miro.medium.com/v2/resize:fit:4800/format:webp/1*KSsGo7ZogfbS0w5DPj-Q8g.png",
+    "title": "Novidades do Angular Framework",
+    "description": "We introduced a developer preview “standalone” feature, which made it possible for the first time to build an application that didn’t rely on NgModules. Since then, standalone has been stabilized, and has become the recommended way to write Angular code by the Angular team. The CLI generates components with standalone: true by default, and the Angular docs teach standalone first to all new Angular developers. Adoption is strong and continues to grow the Angular ecosystem, both in the largest Angular applications written at Google as applications across the internet."
+  },
+  {
+    "id": "2",
+    "photo": "https://www.syncfusion.com/blogs/wp-content/uploads/2022/02/Top-10-Features-of-Angular-13.png",
+    "title": "Saiu a nova versão do Angular",
+    "description": "Angular’s developer preview process enables us to give developers a chance to try out APIs and features before they are promoted to production, and allows us the flexibility to adjust those APIs in response to real-world usage and feedback. Through this feedback on the effect() API during its developer preview, we’ve identified several improvements to the design that will address real issues with both functionality and developer experience. As a result, we’re making meaningful changes to the API that we are excited to share."
+  },
+  {
+    "id": "3",
+    "photo": "https://hermes.dio.me/articles/cover/3eb9c28b-9f4a-45d3-97d9-18d32a38b8c9.jpg",
+    "title": "Angular x Reanct: Uma análise",
+    "description": "Quando se trata de escolher um framework frontend para a construção de aplicações web modernas, Angular e React se destacam como duas das escolhas mais populares. Como engenheiro de software com experiência em ambos os frameworks, tive a oportunidade de mergulhar profundamente em suas arquiteturas, recursos e melhores práticas. Neste artigo, fornecerei uma comparação detalhada do Angular e do React, com foco em recursos importantes, como serviços, rotas, guardas, interceptadores e muito mais, junto com amostras de código para ilustrar seu uso."
+  },
+  {
+    "id": "4",
+    "photo": "https://miro.medium.com/v2/resize:fit:1063/1*zIpFOYkt8f5-FA_v_j38zQ.png",
+    "title": "Signals: O que são? Onde vivem? Como se reproduzem?",
+    "description": "Simply put, a signal is a reactive primitive that represents a value and that allows us to change that same value in a controlled way and track its changes over time. Signals are not a new concept exclusive to Angular. They have been around for years in other frameworks, sometimes under different names."
+  },
+  {
+    "id": "5",
+    "photo": "https://miro.medium.com/v2/resize:fit:720/format:webp/1*0VCw1SdSMbXUci5dQac7Gg.png",
+    "title": "Uptates to effect() in Angular",
+    "description": "Angular’s developer preview process enables us to give developers a chance to try out APIs and features before they are promoted to production, and allows us the flexibility to adjust those APIs in response to real-world usage and feedback. Through this feedback on the effect() API during its developer preview, we’ve identified several improvements to the design that will address real issues with both functionality and developer experience. As a result, we’re making meaningful changes to the API that we are excited to share."
+  },
+]
